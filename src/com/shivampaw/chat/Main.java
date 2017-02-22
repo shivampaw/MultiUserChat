0e742b8894ebea1b4798d88c4899bff432b42938
Shivam Paw <shivam@shivampaw.com>
2017-02-22 23:57:44 +0000

    Better handling for headless desired port

--- src/com/shivampaw/chat/Main.java
+++ src/com/shivampaw/chat/Main.java
@@ -6,7 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if(args.length > 0 && args[0].equals("headless")) {
             System.out.println("Starting headless server");
-            int port = Integer.parseInt(args[1]);
+
+            int port = 0;
+            if(args.length == 2) {
+                try {
+                    port = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    port = 0;
+                }
+            }
+
             new HeadlessServerMain(port);
         } else {
             JavaFXMain.main(new String[]{});
